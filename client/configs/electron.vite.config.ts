c3f823dd901937ff52e1925ca699b4e2cfe1343c
KiosoKen <batistar2@wit.edu>
2024-02-17 15:28:27 -0500

    updating file structure

--- client/configs/electron.vite.config.ts
+++ client/configs/electron.vite.config.ts
@@ -1,18 +1,21 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   main: {
+    root: 'src',
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
+    root: 'src',
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    root: 'src',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('app/renderer/src')
       }
     },
     plugins: [react()]
